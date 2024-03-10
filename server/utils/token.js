fbe63d940ffc36103b0411078e9f115be9c8da57
gagan-chauhan <gagan2020chauhan@gmail.com>
2024-03-10 17:03:58 +0530

    Authentication with cookie sending to frontend done

--- server/utils/token.js
+++ server/utils/token.js
@@ -5,3 +5,11 @@ const JWT_KEY = 'jh17263v27ft32'
 export const generateToken = (payload) => {
     return jwt.sign(payload, JWT_KEY);
 }
+
+export const isAuthenticated = async (req, res, next) => {
+    try {
+
+    } catch (err) {
+        next(err);
+    }
+}
